93918516996cc5727633a8dd800d1b4cb3e45309
lopeztel <lopeztel@gmail.com>
2022-05-05 23:15:26 +0200

    updated script to add current user to corresponding groups

--- install_virt_manager.sh
+++ install_virt_manager.sh
@@ -3,11 +3,11 @@ set -e
 green="\e[0;92m"
 red="\e[0;91m"
 reset="\e[0m"
-CONFIG_FILE=${CONFIG_FILE:-/etc/libvirt/libvirtd.conf}
-SOCKET_GROUP_KEY=${SOCKET_GROUP_KEY:-unix_sock_group}
-SOCKET_GROUP_VALUE=${SOCKET_GROUP_VALUE:-libvirt}
-SOCKET_RW_PERM_KEY=${SOCKET_RW_PERM_KEY:-unix_sock_rw_perms}
-SOCKET_RW_PERM_VALUE=${SOCKET_RW_PERM_VALUE:-0770}
+# CONFIG_FILE=${CONFIG_FILE:-/etc/libvirt/libvirtd.conf}
+# SOCKET_GROUP_KEY=${SOCKET_GROUP_KEY:-unix_sock_group}
+# SOCKET_GROUP_VALUE=${SOCKET_GROUP_VALUE:-libvirt}
+# SOCKET_RW_PERM_KEY=${SOCKET_RW_PERM_KEY:-unix_sock_rw_perms}
+# SOCKET_RW_PERM_VALUE=${SOCKET_RW_PERM_VALUE:-0770}
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
 	echo -e "${red}-I- Not running as root, exit${reset}"
     exit
@@ -25,12 +25,12 @@ systemctl status libvirtd.service
 # sudo sed -n -i "s/\($SOCKET_GROUP_KEY *= *\).*/\1\"$SOCKET_GROUP_VALUE\"/" $CONFIG_FILE
 # sudo sed -n -i "s/\($SOCKET_RW_PERM_KEY *= *\).*/\1\"$SOCKET_RW_PERM_VALUE\"/" $CONFIG_FILE
 # echo -e "${green}-I- Done"
-# echo -e "-I- Modifying group permissions${reset}"
-# sudo usermod -a -G libvirt $(whoami)
-# newgrp libvirt
-# echo -e "${green}-I- Done"
-# echo -e "-I- Restarting libvirtd.service${reset}"
-# sudo systemctl restart libvirtd.service
-# echo -e "${green}-I- Done${reset}"
+echo -e "-I- Modifying group permissions${reset}"
+sudo usermod -a -G libvirt-qemu $(whoami)
+sudo usermod -a -G libvirt $(whoami)
+echo -e "${green}-I- Done"
+echo -e "-I- Restarting libvirtd.service${reset}"
+sudo systemctl restart libvirtd.service
+echo -e "${green}-I- Done${reset}"
 
 #taken from: https://computingforgeeks.com/install-kvm-qemu-virt-manager-arch-manjar/
