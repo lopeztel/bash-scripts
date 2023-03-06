c15692ce5881ea68a72bc99a76d8795863128ced
lopeztel <lopeztel@gmail.com>
2023-03-06 19:57:15 +0100

    naming camera source

--- dslr-webcamstart.sh
+++ dslr-webcamstart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #https://www.youtube.com/watch?v=TsuY4o2zLVQ
-#sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2          #Now done automatically
-gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
-#gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuyv422 -threads 0 -f v4l2 /dev/video0
+# sudo modprobe v4l2loopback exclusive_caps=1 card_label="GPhoto2 Webcam"
+# gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
+gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuyv422 -threads 0 -f v4l2 /dev/video2
